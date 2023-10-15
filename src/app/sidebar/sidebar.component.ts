73b1f6d272aa83fbb0d31855a92e494f715320c0
madhan <manmadhan177@gmail.com>
2023-10-15 12:34:25 +0530

    sidebar list fixed

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,17 +8,32 @@ import{Component}from'@angular/core';
 export class SideBar{
     sidebarWidth: string = '0';
     showEssentialsDropdown: boolean = false;
-
+    showBedroomDropdown:boolean=false;
+    showFoodDropdown:boolean=false;
+    showLaundryDropdown:boolean=false;
+    showFashionDropdown:boolean=false;
   openNav() {
     this.sidebarWidth = '250px';
   }
 
   closeNav() {
     this.sidebarWidth = '0';
-    this.showEssentialsDropdown = false;
+      this.showEssentialsDropdown = false;
 
   }
   toggleEssentialsDropdown() {
     this.showEssentialsDropdown = !this.showEssentialsDropdown;
   }
-}
+  toggleBedroomDropdown() {
+    this.showBedroomDropdown = !this.showBedroomDropdown;
+  }
+  toggleFoodDropdown(){
+    this.showFoodDropdown=!this.showFoodDropdown;
+  }
+  toggleLaundryDropdown(){
+    this.showLaundryDropdown=!this.showLaundryDropdown;
+  }
+  toggleFashionDropdown(){
+    this.showFashionDropdown=!this.showFashionDropdown;
+  }
+} 

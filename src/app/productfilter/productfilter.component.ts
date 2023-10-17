1d1e5558bbbda9474d0475cc20d5dabd0e169aac
madhan <manmadhan177@gmail.com>
2023-10-17 18:01:32 +0530

    filter box and search bar finish

--- src/app/productfilter/productfilter.component.ts
+++ src/app/productfilter/productfilter.component.ts
@@ -6,11 +6,31 @@ import{Component}from '@angular/core';
   })
   export class ProductFilter{
 showCollectionDropdown:boolean=false;
+showSizeDropdown:boolean=false;
+showColorsDropdown:boolean=false;
+showOccassionDropdown:boolean=false;
+showPriceDropdown:boolean=false;
+showSleevesDropdown:boolean=false;
 
     toggleCollectionDropdown(){
       this.showCollectionDropdown = !this.showCollectionDropdown;
     }
-    toggleEssentialsDropdown() {
-      
+    toggleSizeDropdown() {
+      this.showSizeDropdown=!this.showSizeDropdown;
+    }
+    toggleColorsDropdown() {
+      this.showColorsDropdown=!this.showColorsDropdown;
+    }
+    toggleOccassionDropdown() {
+      this.showOccassionDropdown=!this.showOccassionDropdown;
+    }
+    togglePriceDropdown() {
+      this.showPriceDropdown=!this.showPriceDropdown;
+    }
+    toggleDropdown() {
+      this.showSizeDropdown=!this.showSizeDropdown;
+    }
+    toggleSleevesDropdown() {
+      this.showSleevesDropdown=!this.showSleevesDropdown;
     }
   }

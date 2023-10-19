fcd942becd264b673ce17886b7fbcf640edeacbd
madhan <manmadhan177@gmail.com>
2023-10-19 19:38:08 +0530

    address page alignment and data storing work is going on

--- src/app/Accountpage/addresspage/addresspage.component.ts
+++ src/app/Accountpage/addresspage/addresspage.component.ts
@@ -10,7 +10,26 @@ export class AddresspageComponent {
   showEmptyCard: boolean = true; // Initially, the empty card is shown
   profileData: any = {}; // Define your profileData structure as needed
   submittedData: any = null; // Variable to hold the submitted data
+  editedProfileData: any = {};
+  formValid: boolean = false;
+ 
+  editMode: boolean = false;
 
+  // Function to enter edit mode
+  editCard() {
+      // Copy the current data to the editedProfileData variable
+      this.editedProfileData = { ...this.profileData };
+      this.editMode = true;
+  }
+
+  // Function to save the edited data
+  saveCard() {
+      // Update the original profileData with the editedProfileData
+      this.profileData = { ...this.editedProfileData };
+
+      // Exit edit mode
+      this.editMode = false;
+  }
   openForm() {
     this.showForm = !this.showForm; // Toggle the form's visibility
     this.showEmptyCard = !this.showEmptyCard; // Toggle the empty card's visibility
@@ -24,4 +43,7 @@ export class AddresspageComponent {
     this.showForm = false; // Hide the form
     this.showEmptyCard = true; // Show the empty card again after submission
   }
+  
+
+  
 }
